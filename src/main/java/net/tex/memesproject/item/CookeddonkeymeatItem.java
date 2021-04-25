dd8b16d1a516687b43444a13e3cc9e76b865b376
block2333 <3140846162@qq.com>
2021-04-25 19:52:05 +0800

    默认的

---
+++ src/main/java/net/tex/memesproject/item/CookeddonkeymeatItem.java
@@ -0,0 +1,59 @@
+
+package net.tex.memesproject.item;
+
+import net.tex.memesproject.MemesprojectModElements;
+
+import net.minecraftforge.registries.ObjectHolder;
+
+import net.minecraft.world.World;
+import net.minecraft.item.UseAction;
+import net.minecraft.item.Rarity;
+import net.minecraft.item.Items;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.Item;
+import net.minecraft.item.Food;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.entity.LivingEntity;
+
+@MemesprojectModElements.ModElement.Tag
+public class CookeddonkeymeatItem extends MemesprojectModElements.ModElement {
+	@ObjectHolder("memesproject:cookeddonkeymeat")
+	public static final Item block = null;
+	public CookeddonkeymeatItem(MemesprojectModElements instance) {
+		super(instance, 31);
+	}
+
+	@Override
+	public void initElements() {
+		elements.items.add(() -> new FoodItemCustom());
+	}
+	public static class FoodItemCustom extends Item {
+		public FoodItemCustom() {
+			super(new Item.Properties().group(ItemGroup.FOOD).maxStackSize(64).rarity(Rarity.COMMON)
+					.food((new Food.Builder()).hunger(4).saturation(0.3f).setAlwaysEdible().meat().build()));
+			setRegistryName("cookeddonkeymeat");
+		}
+
+		@Override
+		public UseAction getUseAction(ItemStack itemstack) {
+			return UseAction.EAT;
+		}
+
+		@Override
+		public ItemStack onItemUseFinish(ItemStack itemstack, World world, LivingEntity entity) {
+			ItemStack retval = new ItemStack(Items.COOKED_BEEF, (int) (1));
+			super.onItemUseFinish(itemstack, world, entity);
+			if (itemstack.isEmpty()) {
+				return retval;
+			} else {
+				if (entity instanceof PlayerEntity) {
+					PlayerEntity player = (PlayerEntity) entity;
+					if (!player.isCreative() && !player.inventory.addItemStackToInventory(retval))
+						player.dropItem(retval, false);
+				}
+				return itemstack;
+			}
+		}
+	}
+}
